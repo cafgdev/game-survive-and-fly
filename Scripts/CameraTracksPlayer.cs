0ae4f80889f1e1f70fd6e09400dc290cc4a61505
cafgdev <cafg.dev@gmail.com>
2016-08-21 07:25:01 -0300

    Criação do Sprite do Jogador e dos cenarios e inicio do desenvolvimento dos controles do jogador para pc

---
+++ Scripts/CameraTracksPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTracksPlayer : MonoBehaviour {
+
+    Transform player;
+
+    void Start()
+    {
+        GameObject player_go = GameObject.FindGameObjectWithTag("Player");
+        if (player_go == null)
+        {
+            Debug.LogError("Não foi encontrado nenhum objeto com a tag PLayer");
+        }
+        player = player_go.transform;
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    //Variables
+    Vector3 velocity = Vector3.zero;
+    public Vector3 gravity;
+    public Vector3 flapVelocity;
+    public float forwardSpeed =1f;
+    public float maxSpeed = 5f;
+
+
+    bool didFlap = false;
+    
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            didFlap = true;
+            Flap();
+        }
+    }
+
+    public void Flap()
+    {
+        didFlap = false;
+        if (velocity.y < 0)
+            velocity.y = 0;
+        velocity += flapVelocity;
+    }
+
+    public void FixedUpdate()
+    {
+        velocity.x = forwardSpeed;
+        velocity += gravity * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
+        float angle = 0;
+        /*
+        if (velocity.y < 0)
+        {
+            angle = Mathf.Lerp(0, -90, -velocity.y / maxSpeed);
+        }
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+        */
+    }
+
+}
